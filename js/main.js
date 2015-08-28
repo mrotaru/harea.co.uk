1ffccc106073960d6d481742fc0a542bdd83a729
Mihai Rotaru <mihai.rotaru@gmx.com>
2015-08-28 16:50:33 +0100

    write own basic fader

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 $(function(){
-  $('.slider').unslider({
-    delay: 1000,
-    fluid: true,
-    setHeight: false
+
+  window.imageFader = function(opts) {
+
+    var imgs = [];
+
+    function timeout(index) {
+      var toHide = index === 0 ? imgs.length-1 : index-1;
+      imgs[toHide].hide();
+      imgs[index].show();
+      setTimeout(function() {  
+        index = index === imgs.length-1 ? 0 : index+1;
+        timeout(index);
+      }, opts.delay);
+    }
+
+    $('.fader ul li').each(function(){
+      imgs.push($(this));
+      $(this).hide();
+    });
+
+    timeout(0);
+  }
+
+  imageFader({
+    delay: 2000
   });
+
 });
